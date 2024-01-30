08283907511e202be8252ce21e5f33db3dc7d1f1
alfaomega77 <oumaimaalfairam@gmail.com>
2024-01-30 14:48:57 -0800

    bug fixed

--- printf_char.c
+++ printf_char.c
@@ -1,10 +1,6 @@
 #include "main.h"
 
-/**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
- */
+
 int printf_char(va_list val)
 {
 	char s;
--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -1,9 +1,4 @@
 #include "main.h"
-/**
- * printf_exclusive_string - print exclusuives string.
- * @val: argumen t.
- * Return: the length of the string.
- */
 
 int printf_exclusive_string(va_list val)
 {
